800061cbc1523821b41aa45c9a960a403f5a65ec
rizkianakbar <rizkianakbar4@gmail.com>
2024-02-01 17:03:28 +0700

    refactor: rename state and update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,134 @@
-'use client'
+'use client';
 
-import React, {useState} from 'react';
+import { CheckCheck, Edit, Trash2, Undo2 } from 'lucide-react';
+import React, { useState } from 'react';
 
 import { Todo } from '@/lib/types';
+import { cn } from '@/lib/utils';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
-const Home: React.FC = () => {
-    const [task, setTask] = useState<string>('');
-    const [list, setList] = useState<Todo[]>([]);
-  
-    const addTask = () => {
-      const rowNumber = [...list]
-      setList([...list, {id: rowNumber.length +1, value: task, done: false, edit: false}])
-      setTask('')
-    }
-  
-    const markToDone = (id: number) => {
-      setList(list.map((listObj => listObj.id === id ? {...listObj, done: !listObj.done} : listObj)))
-    }
-  
-    const removeTask = (id: number) => {
-      setList(oldValues => {
-        return oldValues.filter(list => list.id !== id)
-      })
-    }
-  
-    const editTask = (id: number) => {
-      const newValue = prompt('edit todo: ');
-      if ( newValue == ''){
-        alert('Task Cannot be empty!')
-      } else if(newValue != null){
-        setList(list.map((listObj => listObj.id === id ? {...listObj, value: newValue, edit: !listObj.edit} : listObj)))
-        setTask('')
-      }
-    }
+const HomePage = (): JSX.Element => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState('');
 
-    const handleEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if(event.key === 'Enter') addTask()
+  const addTask = () => {
+    const rowNumber = [...todos];
+
+    setTodos([
+      ...todos,
+      { id: rowNumber.length + 1, value: inputValue, done: false, edit: false },
+    ]);
+    setInputValue('');
+  };
+
+  const markToDone = (id: number) => {
+    setTodos(
+      todos.map((prevTodos) =>
+        prevTodos.id === id
+          ? { ...prevTodos, done: !prevTodos.done }
+          : prevTodos
+      )
+    );
+  };
+
+  const removeTask = (id: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== id);
+    });
+  };
+
+  const editTask = (id: number) => {
+    const newValue = prompt('edit todo: ');
+    if (newValue == '') {
+      alert('Task Cannot be empty!');
+    } else if (newValue != null) {
+      setTodos(
+        todos.map((prevTodos) =>
+          prevTodos.id === id
+            ? { ...prevTodos, value: newValue, edit: !prevTodos.edit }
+            : prevTodos
+        )
+      );
+      setInputValue('');
     }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addTask();
+    }
+  };
 
   return (
-    <main className='flex flex-col items-center justify-between p-24'>
-      <div className='flex flex-row justify-center items-center'>
-        <Input 
-          className='mx-2 my-4'
+    <main className='max-w-xl mx-auto space-y-4 py-10'>
+      <h1 className='text-2xl font-bold text-center'>To-Do List</h1>
+
+      <div className='flex items-center gap-2'>
+        <Input
           placeholder='Input Task Here'
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-          onKeyDown={(e) => handleEnter(e)}>
-        </Input>
-        <Button 
-          className='mx-2 my-4' 
-          onClick={() => addTask()}>
-            Add Task
-        </Button>
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+
+        <Button onClick={() => addTask()}>Add To-Do</Button>
       </div>
-        <ul className =''>
-          {
-            list.map((listObj) => (
-              <li 
-                className={`${listObj.done ? 'line-through' : ''}`}
-                key={listObj.id}
-                >
-                {listObj.value}
-                <Button 
-                  className='mx-1 my-1'
-                  onClick={() => editTask(listObj.id)}
-                  >
-                    Edit
-                </Button>
-                <Button 
-                  className='mx-1 my-1'
-                  onClick={() => removeTask(listObj.id)}
-                  >
-                    &#10006;
-                </Button>
-                <Button 
-                  className='mx-1 my-1'
-                  onClick={() => markToDone(listObj.id)}
-                  >
-                    {`${listObj.done ? 'Undo' : 'Done'}`}
-                </Button>
-              </li>
-            ))
-          }
-        </ul>
+
+      <ul className='flex flex-col gap-2'>
+        {todos.map((prevTodos) => (
+          <li key={prevTodos.id} className='w-full flex  justify-between gap-2'>
+            <p
+              className={cn(
+                'font-bold max-w-[298px] break-words',
+                prevTodos.done ? 'line-through' : ''
+              )}
+            >
+              {prevTodos.value}
+            </p>
+
+            <div className='flex  gap-1'>
+              <Button
+                className='gap-1'
+                onClick={() => markToDone(prevTodos.id)}
+              >
+                {prevTodos.done ? (
+                  <>
+                    <Undo2 size={16} />
+                    <span>Undone</span>
+                  </>
+                ) : (
+                  <>
+                    <CheckCheck size={16} />
+                    <span>Done</span>
+                  </>
+                )}
+              </Button>
+
+              <Button className='gap-1' onClick={() => editTask(prevTodos.id)}>
+                <Edit size={16} />
+                Edit
+              </Button>
+
+              <Button
+                onClick={() => removeTask(prevTodos.id)}
+                variant='destructive'
+                className='gap-1'
+              >
+                <Trash2 size={16} />
+                Delete
+              </Button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
-}
+};
 
-export default Home;
+export default HomePage;
